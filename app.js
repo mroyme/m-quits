968259a909f1ec399d88d42b5acd2a5a6b19e491
Madhurjya Roy <m@mroy.me>
2023-09-25 11:08:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 
 function getTime() {
-  const fallback = "2023-09-25T20:00"; // 1569369600000, // new Date(2019, 8, 24, 19)
+  const fallback = "2023-09-25T02:30:00.000+05:30"; // 1569369600000, // new Date(2019, 8, 24, 19)
   try {
     const d = Date.parse(params.has("d") ? params.get("d") : fallback);
     return isNaN(d) ? Date.parse(fallback) : d;
